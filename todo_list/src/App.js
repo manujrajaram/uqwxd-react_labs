cfae744d9eea718a842816b7f5460ed4def8f76c
manujrajaram <manujnarra18it@student.mes.ac.in>
2023-06-19 04:08:17 -0400

    Added new task feature

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,9 +3,24 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
-  
+
   // Add the handlesubmit code here
-  
+  function handleSubmit(e){
+      e.preventDefault(); //to prevent browser reload
+
+      const newTodo = {
+          id: new Date().getTime(),
+          text: todo.trim(),
+          completed: false,
+      };
+      if (newTodo.text.length > 0) {
+          setTodos([...todos].concat(newTodo));
+          setTodo("");
+      }else {
+          alert("Enter Valid Task");
+          setTodo("");
+      }
+  }
   
   // Add the deleteToDo code here
 
@@ -17,12 +32,17 @@ const App = () => {
 
   
 return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
+<div id="todo-list">
+    <h1>Todo List</h1>
+    <form onSubmit={handleSubmit}>
+        <input
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+        />
+        <button type="submit">Add Todo</button>
+    </form>
+    {todos.map((todo)=> <div className="todo" key={todo.id}><div>{todo.text}</div></div>)}
 </div>
 );
 };
